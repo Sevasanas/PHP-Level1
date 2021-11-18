4416375e7a9cba3be702203b9d3015ff80e47233
Sevasanas <sevanas86@gmail.com>
2021-11-18 12:25:17 +0300

    question 4

--- lesson2/functions.php
+++ lesson2/functions.php
@@ -15,3 +15,27 @@ function mult ($a, $b){
 function div ($a, $b){
     return ($b === 0) ? "invalid $b value (=0)" : $a / $b;
 }
+
+echo "Первый аргумент: ", $arg1 = rand(-99, 99), "<br>";
+echo "Второй аргумент: ", $arg2 = rand(-99, 99), "<br>";
+echo "Выбранная операция ", $operation = "*", "<br>";
+
+function mathOperation($a, $b, $operation){
+    switch($operation) {
+        case "+" :
+            return sum($a, $b);
+            break;
+        case "-" :
+            return sub($a, $b);
+            break;
+        case "*" :
+            return mult($a, $b);
+            break;
+        case "/" :
+            return div($a, $b);
+            break;
+    }
+}
+
+echo  mathOperation($arg1, $arg2, $operation);
+
