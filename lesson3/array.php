9d1cb2f3b19327e7e3753b4479d3baf61be84701
Sevasanas <sevanas86@gmail.com>
2021-11-23 14:19:38 +0300

    question 8

--- lesson3/array.php
+++ lesson3/array.php
@@ -12,11 +12,30 @@ function outputCity($arr) {
         for($i = 0; $i < $arrLength = count($arr[$key]); $i++) {
             if($i == $arrLength - 1) {
                 $cities = implode(",", $arr[$key]);
-                echo $cities;
+                echo $cities ."<br>";
+            }
+        }
+    }
+}
+
+function searchCity($char, $arr) {
+    $wrongCity = 0;
+    $cityCount = count($arr, COUNT_RECURSIVE) - count($arr);
+    foreach($arr as $key => $value) {
+        for($i = 0; $i < count($arr[$key]); $i++) {
+            $searchLetter = preg_split('//u', $arr[$key][$i], 0, PREG_SPLIT_NO_EMPTY);
+            if($searchLetter[0] == $char) {
+                echo implode($searchLetter) . "<br>";
+            }else{
+                $wrongCity++;
+                if ($wrongCity == $cityCount) {
+                    return print "<br>Города на букву '{$char}' в массиве нет.";
+                }
             }
         }
     }
 }
 
 outputCity($districts);
+searchCity('К', $districts);
 
